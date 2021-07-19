f40f72618e0fc81a6723ed13c7d2667add8fd8b1
DaniilAutoQA <daniil-bond@yandex.ru>
2021-07-19 08:51:52 +0300

    изменен тест расчета депозита из-за изменения ставки

--- src/test/java/cloud/autotests/tests/GazpromBankTests.java
+++ src/test/java/cloud/autotests/tests/GazpromBankTests.java
@@ -69,14 +69,14 @@ public class GazpromBankTests extends TestBase {
             $(".nn-checkbox").click();
         });
         step("Проверить рассчет", () -> {
-            step("Сумма в конце срока должна быть  9 494 092", () ->{
-                $(".js-nn-calc-rSum").shouldHave(text("9 494 092"));
+            step("Сумма в конце срока должна быть  9 528 480", () ->{
+                $(".js-nn-calc-rSum").shouldHave(text("9 528 480"));
             });
-            step("Доход должен быть 494 092", () ->{
-                $(".js-nn-calc-rProfit").shouldHave(text("494 092"));
+            step("Доход должен быть 528 480", () ->{
+                $(".js-nn-calc-rProfit").shouldHave(text("528 480"));
             });
-            step("Процентная ставка должна быть 5.46%", () ->{
-                $(".js-nn-calc-rRate").shouldHave(text("5.46"));
+            step("Процентная ставка должна быть 5.84%", () ->{
+                $(".js-nn-calc-rRate").shouldHave(text("5.84"));
             });
             step("Эффективная ставка не должна отображаться", () ->{
                 $(".js-nn-calc-effective").shouldNot(visible);
